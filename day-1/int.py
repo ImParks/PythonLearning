6eaa9875f36801b792d5dc47c6af969a833a6a95
ImPark <jaeyeol0311@gmail.com>
2024-09-19 23:17:32 +0900

    [임재열] 2024-09-19 > 파이썬 공부 1일차 8진수

---
+++ day-1/int.py
@@ -0,0 +1,39 @@
+# 정수 자료형
+
+a = 123
+print(a)
+print(type(a))
+a = 0
+print(a)
+print(type(a))
+a = -213
+print(a)
+print(type(a))
+
+# 실수 자료형
+a = 1.2
+print(a)
+print(type(a))
+
+a = -3.45
+print(a)
+print(type(a))
+
+a = 0.0
+print(a)
+print(type(a))
+
+# 컴퓨터식 지수 표현방식
+a = 4.24e10
+print(a)
+print(type(a))
+
+a = 4.24e-10
+print(a)
+print(type(a))
+
+
+# 8진수와 16진수
+a = 0o177
+print(a)
+print(type(a))
