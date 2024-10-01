ee727592ebe5624e67786fa01e591abb1e117b81
ImPark <jaeyeol0311@gmail.com>
2024-10-01 15:24:45 +0900

    [임재열] 2024-10-01 > 포맷 코드와 숫자 함께 사용하기

--- day-2/String-4.py
+++ day-2/String-4.py
@@ -17,7 +17,7 @@ print(c)
 
 #복수 변수대입
 day = "three"
-d = "I eat %d apples. so   I was sick for %s days." % (number,day)
+d = "I eat %d apples. so I was sick for %s days." % (number,day)
 print(d)
 
 # 문자열 포맷 코드
@@ -28,3 +28,17 @@ print(d)
 # %o : 8진수
 # %x : 16진수
 # %% : Literal%(문자 % 자체)
+
+#팁 : %s 만 사용해도어떤 형태의 값이든 변환하여 집어넣기 가능
+e = "I eat %s apples. so I was sick for %s days." % (number,day)
+print(e)
+
+#포맷 코드와 숫자 함께 사용하기
+
+#문자열 전체 길이가 %10s <- 이 걸로인해 10칸으로 정해짐
+#문자가 뒤로 오고 전체 길이가 10칸
+f = "%10s" %"hi"
+print(f)
+#문자가 앞으로오고 전체 길이가 10칸
+g = "%-10s" %"hi"
+print(g)
