437f1665c5b70dcf665923dbeddb2292398d8a2c
ImPark <jaeyeol0311@gmail.com>
2024-10-01 15:05:06 +0900

    [임재열] 2024-10-01 > 인덱싱 2

--- day-2/String-3.py
+++ day-2/String-3.py
@@ -9,3 +9,9 @@ print(len(a))
 #문자열 인덱싱
 a = "Life is too short, You need Python"
 print(a[3])
+# 특정 index 값에 있는 문자를 가져온다.
+# index 값은 0부터 시작
+print(a[0])
+print(a[12])
+# 음수 일때는 뒤에서 시작
+print(a[-1])
