85c921ec4bd3aa9676ed2f7f8a05da97a7034f5a
ImPark <jaeyeol0311@gmail.com>
2024-10-01 14:57:07 +0900

    [임재열] 2024-10-01 > 파이썬 문자열 및 이스케이프 코드

---
+++ day-2/String.py
@@ -0,0 +1,9 @@
+a = "Life is too short, You need Python"
+a2 = 'Life is too short, You need Python'
+a3 = '''Life is too short, You need Python'''
+a4 = """Life is too short, You need Python"""
+a5 = "Life is too short, \nYou need Python"
+b = "a"
+c = "123"
+print(type(a))
+print(a5)
