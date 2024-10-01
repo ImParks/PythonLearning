5f9b4455123e1bacb1961a043639abbd71830a47
ImPark <jaeyeol0311@gmail.com>
2024-10-01 15:19:50 +0900

    [임재열] 2024-10-01 > 문자열 포맷 코드

--- day-2/String-4.py
+++ day-2/String-4.py
@@ -17,5 +17,14 @@ print(c)
 
 #복수 변수대입
 day = "three"
-d = "I eat %d apples. so I was sick for %s days." % (number,day)
+d = "I eat %d apples. so   I was sick for %s days." % (number,day)
 print(d)
+
+# 문자열 포맷 코드
+# %s : 문자열
+# %c : 문자열 1개
+# %d : 정수
+# %f : 부동소수
+# %o : 8진수
+# %x : 16진수
+# %% : Literal%(문자 % 자체)
