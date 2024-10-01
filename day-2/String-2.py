5ea7003fd4afed8af3b8490251002563b22f782a
ImPark <jaeyeol0311@gmail.com>
2024-10-01 14:59:41 +0900

    [임재열] 2024-10-01 > 문자열 곱하기

--- day-2/String-2.py
+++ day-2/String-2.py
@@ -1,3 +1,11 @@
 head = "Python"
 tail = " is fun!"
 print(head + tail)
+#문자열 끼리 연결의 개념
+print(head*3)
+#문자열 곱하기(문자열 * 숫자) 를 하게되면 int만큼 반복하는 문자열을 얻게됨 문자열 * 문자열은 불가능
+
+#응용
+print("="*50)
+print("My Program")
+print("="*50)
